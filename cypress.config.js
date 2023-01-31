f1440edbf4f48111ea2a40f0d6eb3b1b3aef2c9a
Mohammad M Islam <mmislam020409@gmail.com>
2023-01-31 08:48:03 +0600

    config file updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,17 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('@cypress/grep/src/plugin')(config);
+      require('cypress-mochawesome-reporter/plugin')(on);
       allureWriter(on, config);
       return config;
     }
   },
+
   // env: {
   //   "grep" : "regression",
   //   "browser" : "firefox",
